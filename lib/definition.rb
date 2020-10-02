93fa993cf5a3f44d71320a5a5b4c0a4e71069fe8
Brycen Bartolome <brycenbartolome@gmail.com>
2020-10-02 11:21:22 -0700

    fail test, will show an array of all defitions

---
+++ lib/definition.rb
@@ -0,0 +1,20 @@
+class Definition
+
+  attr_accessor :def, :word_id, :id
+
+  @@definitions = {}
+  @@rows = 0
+ 
+  def initialize(att)
+    @def = att.fetch(:def)
+    @word_id = att.fetch(:word_id)
+    @id = att.fetch(:id) || @@rows +=1
+  end
+
+  def self.clear
+    @@definitions = {}
+    @@rows = 0
+  end
+end
+
+  
--- spec/definition_spec.rb
+++ spec/definition_spec.rb
@@ -3,4 +3,14 @@ require 'definition'
 
 describe('Definition') do
   
+  before(:each) do
+    @att = {:def => 'something super cool', :word_id => 1, :id => nil}
+    Definition.clear
+  end
+
+  describe('.all') do
+    it('will show an array of all defitions') do
+      expect(Definition.all).to(eq(['hello']))
+    end
+  end
 end
