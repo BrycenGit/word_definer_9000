ab561cfa211743448951ff08f70480529504b1b6
Brycen Bartolome <brycenbartolome@gmail.com>
2020-10-02 11:25:44 -0700

    fail test,definition will be equal if name, id, and word_id are the same

--- spec/definition_spec.rb
+++ spec/definition_spec.rb
@@ -13,4 +13,12 @@ describe('Definition') do
       expect(Definition.all).to(eq([]))
     end
   end
+
+  describe('#==') do
+    it('definition will be equal if name, id, and word_id are the same') do
+      def1 = Definition.new({:def => 'something super cool', :word_id => 1, :id => 1})
+      def2 = Definition.new({:def => 'something super cool', :word_id => 1, :id => 1})
+      expect(def1).to(eq(def2))
+    end
+  end
 end
